952df591d51d5fb8fc02800510a3b8a0f488fd7c
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2020-05-17 19:41:59 +0200

    Added app

---
+++ SEDC.VideoRental/SEDC.VideoRental.App/Program.cs
@@ -0,0 +1,92 @@
+﻿using SEDC.VideoRental.App.Menus;
+using SEDC.VideoRental.Data.Models;
+using SEDC.VideoRental.Services.Helpers;
+using SEDC.VideoRental.Services.Menus;
+using SEDC.VideoRental.Services.Services;
+using System;
+
+namespace SEDC.VideoRental.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "VideoRental";
+            
+            // global variable that we will reuse thru the application
+            var userService = new UserService();
+            var movieService = new MovieService();
+            User user = null;
+            string errorMessage = string.Empty;
+
+            #region Login/SignUp
+            Screen.HomeScreen();
+            bool isLoggedIn = false;
+            while(!isLoggedIn)
+            {
+                Screens.StartMenu();
+                var startMenuInput = InputParser.ToInteger(1, 3);
+                switch (startMenuInput)
+                {
+                    case 1:
+                        while (!isLoggedIn) 
+                        {
+                            user = userService.Login();
+                            isLoggedIn = !isLoggedIn;
+                        }
+                        break;
+                    case 2:
+                        while (!isLoggedIn)
+                        {
+                            user = userService.SignUp();
+                            isLoggedIn = !isLoggedIn;
+                        }
+                        break;
+                    case 3:
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+            #endregion
+
+
+            #region ViewSection
+
+            //TODO: next class
+            while (true)
+            {
+                Screen.ClearScreen();
+                Screen.ErrorMessage(errorMessage);
+                Screen.MainMenu(user.FullName);
+                var selection = InputParser.ToInteger(1, 4);
+                switch (selection)
+                {
+                    case 1:
+                        movieService.ViewMovieList(user);
+                        break;
+                    case 2:
+                        // TODO: PART 3
+                        movieService.ViewRentedVideos(user);
+                        // TODO: END PART 3
+                        break;
+                    case 3:
+                        // TODO: View history
+                        try
+                        {
+                            movieService.ViewRentedHistoryVideos(user);
+                        }
+                        catch (Exception ex)
+                        {
+                            errorMessage = ex.Message;
+                        }
+                        break;
+                    case 4:
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+            // till here 
+            #endregion
+        }
+    }
+}
---
+++ SEDC.VideoRental/SEDC.VideoRental.Data/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using SEDC.VideoRental.Data.BaseModels;
+using System;
+using System.Collections.Generic;
+
+namespace SEDC.VideoRental.Data.Models
+{
+    public class User : BaseEntity
+    {
+        public string FullName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        // card number is used as a username
+        public int CardNumber { get; set; }
+        public bool IsSubscriptionExpired { get; set; }
+        
+        // TODO: PART 3
+        public List<RentalInfo> RentedMovies { get; set; }
+        public List<RentalInfo> RentedMoviesHistory { get; set; }
+
+        // TODO : END PART 3
+        public User()
+        {
+            IsSubscriptionExpired = false;
+            RentedMovies = new List<RentalInfo>();
+            RentedMoviesHistory = new List<RentalInfo>();
+        }
+    }
+}
